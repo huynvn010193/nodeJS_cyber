33c028d8b970288570172030bbd31cbd8bf441a1
huynvn010193 <hpn010193@gmail.com>
2021-12-14 23:52:49 +0700

    bai06: CRUD command nodejs

--- nodejs-todolist/app/index.js
+++ nodejs-todolist/app/index.js
@@ -10,5 +10,78 @@ yargs.command({
   },
 });
 
+//create - node app/index.js create --title="Hoc nodeJS" --description="Dau kho lam dau"
+yargs.command({
+  command: "create",
+  builder: {
+    title: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { title, description } = args;
+    console.log(title, description);
+  },
+});
+
+// read-all - node app/index.js read-all
+yargs.command({
+  command: "read-all",
+  handler: () => {
+    console.log("read-all");
+  },
+});
+
+// read-detail - node app/index.js read-detail --id="123"
+yargs.command({
+  command: "read-detail",
+  builder: {
+    id: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { id } = args;
+    console.log("read-detail id", id);
+  },
+});
+
+// update - node app/index.js update --id="123" --title="Hoc JS" --description="kho lam"
+yargs.command({
+  command: "update",
+  builder: {
+    id: {
+      type: "string",
+    },
+    title: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { id, title, description } = args;
+    console.log("update id title des", id, title, description);
+  },
+});
+
+// delete - node app/index.js delete --id="123"
+yargs.command({
+  command: "delete",
+  builder: {
+    id: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { id } = args;
+    console.log("delete id", id);
+  },
+});
+
 // lưu lại các lệch vừa tạo
 yargs.parse();
