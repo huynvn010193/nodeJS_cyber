824cb03ec313ea69c868772a4a8f3ecd9e1dd324
huynvn010193 <hpn010193@gmail.com>
2021-12-14 23:15:17 +0700

    bai05: Create command nodeJS

--- nodejs-todolist/app/index.js
+++ nodejs-todolist/app/index.js
@@ -1,1 +1,14 @@
-console.log("đây là code bưởi 2")
+// import yargs from "yargs"; // es6
+
+const yargs = require("yargs"); // es5
+
+// tạo lệnh
+yargs.command({
+  command: "test",
+  handler: () => {
+    console.log("test");
+  },
+});
+
+// lưu lại các lệch vừa tạo
+yargs.parse();
