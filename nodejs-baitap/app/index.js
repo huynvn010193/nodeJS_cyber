17fb1eef2a860baf203bbfa5452d95fcc13ca0f2
huynvn010193 <hpn010193@gmail.com>
2021-12-19 23:29:00 +0700

    Update cpde

--- nodejs-baitap/app/index.js
+++ nodejs-baitap/app/index.js
@@ -3,7 +3,11 @@ const yargs = require("yargs"); // es5
 const fs = require("fs"); // file system (build in nodeJS);
 
 const {
-  readAllProduct
+  deleteProduct,
+  readAllProduct,
+  readDetailProduct,
+  createProduct,
+  updateProduct,
 } = require("./model/product");
 
 // node app/index.js test
@@ -13,8 +17,6 @@ yargs.command({
     console.log("test");
   },
 });
-
-
 
 // create - node app/index.js test
 yargs.command({
@@ -30,6 +32,98 @@ yargs.command({
   handler: () => {
     const result = readAllProduct();
     console.log(chalk.blue("All-Product"), result);
+  },
+});
+
+// read-detail - node app/index.js read-detail --id="1"
+yargs.command({
+  command: "read-detail",
+  builder: {
+    id: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { id } = args;
+    const product = readDetailProduct(id);
+    if (product) {
+      console.log("product: ", product);
+    } else {
+      console.log(chalk.yellow("Not found"));
+    }
+  },
+});
+
+// create - node app/index.js create --name="sam pham 03" --price=3000 --amount=1 --description="san pham 03"
+yargs.command({
+  command: "create",
+  builder: {
+    name: {
+      type: "string",
+    },
+    price: {
+      type: "number",
+    },
+    amount: {
+      type: "number",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { title, price, amount, description } = args;
+    const newProduct = createProduct(title, price, amount, description);
+  },
+});
+
+// update - node app/index.js update --name="sam pham 003" --price=2000 --amount=2 --description="san pham 03"
+yargs.command({
+  command: "update",
+  builder: {
+    id: {
+      type: "string",
+    },
+    name: {
+      type: "string",
+    },
+    price: {
+      type: "number",
+    },
+    amount: {
+      type: "number",
+    },
+    description: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { id, name, price, amount, description } = args;
+    const product = updateProduct(id, name, price, amount, description);
+    if (product) {
+      console.log("product updated: ", product);
+    } else {
+      console.log("Not found");
+    }
+  },
+});
+
+// delete - node app/index.js delete --id="1"
+yargs.command({
+  command: "delete",
+  builder: {
+    id: {
+      type: "string",
+    },
+  },
+  handler: (args) => {
+    const { id } = args;
+    const product = deleteProduct(id);
+    if (product) {
+      console.log("product deleted: ", product);
+    } else {
+      console.log("Not found");
+    }
   },
 });
 
--- nodejs-baitap/app/model/product.js
+++ nodejs-baitap/app/model/product.js
@@ -10,6 +10,62 @@ const readAllProduct = () => {
   return productJson;
 };
 
+const readDetailProduct = (id) => {
+  let productList = readAllProduct();
+  const prdduct = productList.find((product) => id === product.id);
+  return prdduct;
+};
+
+const createProduct = (name, price, amount, description) => {
+  const newProduct = {
+    id: Math.random().toString(),
+    name,
+    price,
+    amount,
+    description,
+  };
+  let productList = readAllProduct();
+  productList = [...productList, newProduct];
+  fs.writeFileSync("product.json", JSON.stringify(productList));
+  return newProduct;
+};
+
+const updateProduct = (id, name, price, amount, description) => {
+  let productList = readAllProduct();
+  const index = productList.findIndex((task) => id === task.id);
+  if (index !== -1) {
+    // thực hiện update
+    const oldProduct = productList[index];
+    const newProduct = { ...oldProduct, name, price, amount, description };
+    productList[index] = newProduct;
+    fs.writeFileSync("product.json", JSON.stringify(productList));
+    return newProduct;
+  } else {
+    // thông báo cho người dùng bik
+    return false;
+  }
+};
+
+const deleteProduct = (id) => {
+  let productList = readAllProduct();
+  const index = productList.findIndex((product) => id === product.id);
+  if (index !== -1) {
+    // thực hiện deleta
+    const product = productList[index];
+    productList = productList.filter((product) => product.id !== id);
+    fs.writeFileSync("product.json", JSON.stringify(productList));
+
+    return product;
+  } else {
+    // thông báo cho người dùng bik
+    return false;
+  }
+};
+
 module.exports = {
+  deleteProduct,
+  updateProduct,
+  createProduct,
   readAllProduct,
+  readDetailProduct,
 };
