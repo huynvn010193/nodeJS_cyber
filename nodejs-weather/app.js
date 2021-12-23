e971e1007b2abe37d3d2f32015c0a130b1213a5b
huynvn <Huy.Nguyen@navigosgroup.com>
2021-12-23 23:31:29 +0700

    P2_B4: Get api weather

--- nodejs-weather/app.js
+++ nodejs-weather/app.js
@@ -1,1 +1,29 @@
-console.log("cyber weather app 02");
+const asyncRequest = require("async-request");
+
+const getWeather = async(location) => {
+  const access_key = "c820e73dd2c90cf79724c46989b208da"
+  const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${location}`;
+  try {
+    const res = await asyncRequest(url);
+    const data = JSON.parse(res.body);
+    const weather = {
+      issuccess: true,
+      region: data.location.region,
+      country: data.location.country,
+      temperature: data.current.temperature,
+      wind_speed: data.current.wind_speed,
+      precip: data.current.precip,
+      cloudcover: data.current.cloudcover,
+    }
+    console.log("res",weather);
+    return weather;
+  } catch (err) {
+    console.log("res",err);
+    return {
+      issuccess: false,
+      err
+    }
+  }
+};
+
+getWeather("tokyo");
