0694f10bb33dba14e760e5310fc2c4f74b8b1d90
huynvn010193 <hpn010193@gmail.com>
2022-01-15 00:34:18 +0700

    P4_B3: Cross join

--- mysql_relationship.sql
+++ mysql_relationship.sql
@@ -12,6 +12,11 @@ create table customers(
     birthday date not null
 );
 
+insert into customers(first_name, last_name, email, birthday)
+values ('Hào','Nguyễn','hao@gmail.com','1997-05-11'),
+('Thảo','Trương','thaotruong@gmail.com','1999-11-12'),
+('Hường','Lê','huong@gmail.com','1997-05-11');
+
 -- tạo bảng orders -- tạo bảng nhiều (trong mối quan hệ 1 = n)
 create table orders(
 	order_id int primary key auto_increment,
@@ -22,3 +27,12 @@ create table orders(
     foreign key (customer_id) references customers(customer_id)
 );
 
+-- thêm data vào table
+insert into orders(order_date, price, amount, customer_id)
+values("2021-02-17", 1000, 80, 1),
+("2021-02-13", 1500, 50, 2),
+("2021-01-17", 500, 10, 1);
+
+-- kỹ thuật cross join
+-- Tìm ra đơn đặt hàng mà khách hàng Hào đã đặt
+select * from orders where customer_id = (select customer_id from customers where first_name = "Hào");
