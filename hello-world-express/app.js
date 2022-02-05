b85bd9483b1480641922e64745c21732d56c5965
huynvn <Huy.Nguyen@navigosgroup.com>
2022-02-05 11:53:54 +0700

    P5_Bai04: Router get list student

--- hello-world-express/app.js
+++ hello-world-express/app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Lấy danh sách học sinh (url: http://localhost:3000/students)
+app.get('/students', (req, res) => {
+  res.send('Lấy danh sách học sinh');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
