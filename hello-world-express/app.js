c847721401803aa4e7253bee82b4bc11c3418e3c
huynvn <Huy.Nguyen@navigosgroup.com>
2022-02-08 15:49:20 +0700

    P5_Bai6:Router create student

--- hello-world-express/app.js
+++ hello-world-express/app.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// chuyển req, res về dạng json tiện thao tác
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -16,7 +19,14 @@ app.get('/students/:id', (req, res) => {
   const params = req.params;
   const id = params.id;
   res.send('lấy thông tin chi tiết của học sinh có id là: ' +id);
-})
+});
+
+// thêm danh sách học sinh
+app.post("/students", (req, res) => {
+  const student = req.body;
+  console.log(req.body);
+  res.send('thêm học sinh');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
