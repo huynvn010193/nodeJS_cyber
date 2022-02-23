c3fc72a02adb94d85d041d42a911214328640cc1
huynvn010193 <hpn010193@gmail.com>
2022-02-23 23:03:15 +0700

    P5_B18:Services structer folder

--- hello-world-express/app/controllers/student.controllers.js
+++ hello-world-express/app/controllers/student.controllers.js
@@ -1,38 +1,27 @@
-let studentList = [
-  {
-    id: 1,
-    fullName: "Nguyễn Phong Hào",
-    age: 18,
-    numberClass: 12,
-  },
-  {
-    id: 2,
-    fullName: "Nguyễn Văn Bưởi",
-    age: 17,
-    numberClass: 11,
-  },
-  {
-    id: 3,
-    fullName: "Lê Thị Chuối",
-    age: 15,
-    numberClass: 9,
-  },
-];
+const {
+  getList,
+  getDetail,
+  create,
+  update,
+  deleteById,
+} = require("../services/student.services");
 
 const getStudentList = (req, res) => {
-  res.status(200).send(studentList);
+  const studentList = getList();
+  if (studentList) {
+    res.status(200).send(studentList);
+  } else {
+    res.status(404).send("Not Found");
+  }
 };
 
 const getStudentDetailById = (req, res) => {
   const params = req.params;
   const id = params.id;
 
-  const index = studentList.findIndex((student) => {
-    return student.id == id;
-  });
+  const student = getDetail(id);
 
-  if (index !== -1) {
-    const student = studentList[index];
+  if (student) {
     res.status(200).send(student);
   } else {
     res.status(404).send("Not found");
@@ -41,33 +30,30 @@ const getStudentDetailById = (req, res) => {
 
 const createStudent = (req, res) => {
   let student = req.body;
-  // Tạo id không dc trùng
-  student = { id: Math.random(), ...student };
-  studentList = [...studentList, student];
-  res.status(201).send(student);
+
+  const newStudent = create(student);
+  res.status(201).send(newStudent);
 };
 
 const updateStudentById = (req, res) => {
   const { id } = req.params;
-  const { fullName, age, numberClass } = req.body;
-  const index = studentList.findIndex((student) => student.id == id);
-  if (index !== -1) {
-    const oldStudent = studentList[index];
-    const updatedStudent = { ...oldStudent, fullName, age, numberClass };
-    studentList[index] = updatedStudent;
-    res.status(200).send(updatedStudent);
+  const student = req.body;
+
+  const studentUpdated = update(id, student);
+
+  if (studentUpdated) {
+    res.status(200).send(studentUpdated);
   } else {
-    res.status(404).send("Not Found");
+    res.status(404).send("Not found");
   }
 };
 
 const deleteStudentById = (req, res) => {
   const { id } = req.params;
-  const index = studentList.findIndex((student) => student.id == id);
-  if (index !== -1) {
-    const student = studentList[index];
-    studentList.splice(index, 1);
-    res.status(200).send(studentList);
+  const student = deleteById(id);
+
+  if (student) {
+    res.status(200).send(student);
   } else {
     res.status(404).send("Not found");
   }
---
+++ hello-world-express/app/services/student.services.js
@@ -0,0 +1,79 @@
+let studentList = [
+  {
+    id: 1,
+    fullName: "Nguyễn Phong Hào",
+    age: 18,
+    numberClass: 12,
+  },
+  {
+    id: 2,
+    fullName: "Nguyễn Văn Bưởi",
+    age: 17,
+    numberClass: 11,
+  },
+  {
+    id: 3,
+    fullName: "Lê Thị Chuối",
+    age: 15,
+    numberClass: 9,
+  },
+];
+
+const getList = () => {
+  if (studentList) {
+    return studentList;
+  } else {
+    return false;
+  }
+};
+
+const getDetail = (id) => {
+  const index = studentList.findIndex((student) => {
+    return student.id == id;
+  });
+
+  if (index !== -1) {
+    const student = studentList[index];
+    return student;
+  } else {
+    return false;
+  }
+};
+
+const create = (student) => {
+  // Tạo id không dc trùng
+  const newStudent = { id: Math.random(), ...student };
+  studentList = [...studentList, newStudent];
+  return newStudent;
+};
+
+const update = (id, student) => {
+  const index = studentList.findIndex((student) => student.id == id);
+  if (index !== -1) {
+    const oldStudent = studentList[index];
+    const studentUpdated = { ...oldStudent, ...student };
+    studentList[index] = studentUpdated;
+    return studentUpdated;
+  } else {
+    return false;
+  }
+};
+
+const deleteById = (id) => {
+  const index = studentList.findIndex((student) => student.id == id);
+  if (index !== -1) {
+    const student = studentList[index];
+    studentList.splice(index, 1);
+    return student;
+  } else {
+    return false;
+  }
+};
+
+module.exports = {
+  getList,
+  getDetail,
+  create,
+  update,
+  deleteById,
+};
