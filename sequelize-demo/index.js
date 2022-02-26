5d803224627abf152355617acd4ea4f159af84c5
huynvn <Huy.Nguyen@navigosgroup.com>
2022-02-26 23:13:53 +0700

    P6_B3: Model

--- sequelize-demo/index.js
+++ sequelize-demo/index.js
@@ -1,9 +1,33 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("task_management","root","admin23!%",{
   host: "localhost",
   dialect: "mysql"
 });
+
+// Tạo model
+const Task = sequelize.define(
+  'Task',
+  {
+    name: {
+      type: DataTypes.STRING, // VARCHAR(255)
+      allowNull: false, // Không bao giờ dc null
+    },
+    status: {
+      type: DataTypes.STRING,
+    }
+  }
+);
+
+// Đồng bộ model
+const syncModel = async() => {
+  // force: true nghĩa là model thay đổi thì bảng đó bị xoá đi tạo ra bảng mới.
+  await Task.sync({force: true});
+  // Task.sync({alter: true}); // Không xoá bảng cũ mà chỉ sửa bảng cũ thành bảng mới.
+  console.log("Đã đồng bộ model task")
+}
+
+syncModel();
 
 const checkConnect = async () => {
   try {
@@ -13,5 +37,6 @@ const checkConnect = async () => {
     console.log("Kết nối thất bại", error);
   }
 };
+
 
 checkConnect();
