65db99133580aa466140a640f60f9977d421bc2a
huynvn010193 <hpn010193@gmail.com>
2022-03-07 21:47:29 +0700

    P7_Bai7: Model Migrate

---
+++ vexere/migrations/20220307142113-create-station.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // Chịu trách nhiệm tạo bảng trong database
+    await queryInterface.createTable("Stations", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      provinces: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  // Chịu trách nhiệm xóa bảng khi nâng cấp version bảng ko còn tồn tại nữa khi chạy lệnh: yarn sequelize-cli db:migrate:undo
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Stations");
+  },
+};
