53535f13f5248070afab6fdd1801b408355f1c72
huynvn010193 <hpn010193@gmail.com>
2022-03-15 21:37:46 +0700

    P7_B17: Register User

--- vexere/controllers/user.controllers.js
+++ vexere/controllers/user.controllers.js
@@ -1,15 +1,27 @@
-const { User } = require('../models');
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
-const register = async(req, res) => {
+const register = async (req, res) => {
   const { name, email, password, numberPhone } = req.body;
   try {
-    const newUser = await User.create({ name, email, password, numberPhone, type: "client" });
+    // tạo ra 1 chuỗi ngẩu nhiên
+    // genSaltSync : tạo đồng bộ -> tạo xong mới chạy tiếp
+    const salt = bcrypt.genSaltSync(10);
+
+    // mã hóa chuỗi ngẫu nhiên vừa mới tạo ra : salt + password
+    const hashPassword = bcrypt.hashSync(password, salt);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      numberPhone,
+    });
     res.status(201).send(newUser);
   } catch (error) {
     res.status(500).send(error);
   }
-}
+};
 
 module.exports = {
-  register
-}
+  register,
+};
--- vexere/migrations/20220314132245-create-user.js
+++ vexere/migrations/20220314132245-create-user.js
@@ -1,40 +1,41 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable("users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       numberPhone: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue: "CLIENT",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('users');
-  }
+    await queryInterface.dropTable("users");
+  },
 };
