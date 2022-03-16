198d25e1eb5241e086a0b3572a3bf0ebb80898ce
huynvn010193 <hpn010193@gmail.com>
2022-03-16 23:49:48 +0700

    P7_Bai19: Token

--- vexere/controllers/user.controllers.js
+++ vexere/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { name, email, password, numberPhone } = req.body;
@@ -35,7 +36,12 @@ const login = async (req, res) => {
   if (user) {
     const isAuth = bcrypt.compareSync(password, user.password);
     if (isAuth) {
-      res.status(200).send({ message: "Đăng nhập thành công!" });
+      const token = jwt.sign(
+        { email: user.email, type: user.type },
+        "huy-nhat-2309",
+        { expiresIn: 60 * 60 }
+      );
+      res.status(200).send({ message: "Đăng nhập thành công!", token });
     } else {
       res.status(500).send({ message: "Đăng nhập thất bại!" });
     }
