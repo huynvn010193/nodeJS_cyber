98ea0e49182b543000f4d4e8be22403f025cfda8
huynvn <Huy.Nguyen@navigosgroup.com>
2022-03-18 22:07:11 +0700

    P7_Bai20: Athenticate

---
+++ vexere/middlewares/auth/authenticate.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const authenticate = (req, res, next) => {
+  try {
+    const token = req.header("token");
+    const decode = jwt.verify(token, "huy-nhat-2309");
+    if(decode) {
+      req.user = decode;
+      return next();
+    } else {
+      res.status(401).send("Bạn chưa đăng nhập xác thực người dùng")
+    }
+  } catch (error) {
+    res.status(500).send(error)
+  }
+};
+
+module.exports = {
+  authenticate
+}
--- vexere/routers/station.routers.js
+++ vexere/routers/station.routers.js
@@ -8,14 +8,15 @@ const {
   deleteStation,
 } = require("../controllers/station.controllers");
 const { checkExist } = require("../middlewares/validations/checkExist");
+const { authenticate } = require("../middlewares/auth/authenticate");
 
 const stationRouter = express.Router();
 
-stationRouter.post("/", createStation);
+stationRouter.post("/", authenticate, createStation);
 stationRouter.get("/", getAllStation);
 stationRouter.get("/:id", getDetailStation);
 stationRouter.put("/:id", checkExist(Station), updateStation);
-stationRouter.delete("/:id", checkExist(Station), deleteStation);
+stationRouter.delete("/:id",authenticate, checkExist(Station), deleteStation);
 
 module.exports = {
   stationRouter,
