7885cec68a00c3d147da30744627ac2e73c921d1
huynvn <Huy.Nguyen@navigosgroup.com>
2022-03-19 10:59:09 +0700

    P7_B22: Upload file multer basic

--- vexere/routers/user.routers.js
+++ vexere/routers/user.routers.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { register, login } = require("../controllers/user.controllers");
 
+
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+
+// upload file
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/avatars"})
+
+userRouter.post("/upload-avatar", upload.single('avatar'), (req, res) => {
+  res.send("Tính năng upload file run");
+})
 
 module.exports = {
   userRouter,
