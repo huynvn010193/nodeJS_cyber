2ecaa97a0a0098d6fd0ad02fa7029469a3dc6be4
huynvn010193 <hpn010193@gmail.com>
2022-03-22 22:19:40 +0700

    P7_B35: Ticket migration

--- vexere/models/user.js
+++ vexere/models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,20 +7,24 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Ticket }) {
       // define association here
+      this.hasMany(Ticket, { foreignKey: "user_id" });
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    numberPhone: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    type: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      numberPhone: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      type: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
