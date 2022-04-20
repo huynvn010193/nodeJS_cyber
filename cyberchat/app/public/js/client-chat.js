da600cff130a6f2a604a0723b656ef6bda295298
huynvn010193 <hpn010193@gmail.com>
2022-04-20 23:52:23 +0700

    B10: Phut 08 giay 15

--- cyberchat/app/public/js/client-chat.js
+++ cyberchat/app/public/js/client-chat.js
@@ -4,14 +4,25 @@ document.getElementById("form-message").addEventListener("submit", (e) => {
   e.preventDefault();
   const messageText = document.getElementById("input-message").value;
   const acknowledgements = (errors) => {
-    if(errors) {
+    if (errors) {
       return alert("tin nhắn không hợp lệ");
     }
     console.log("Bạn đã gửi tin nhắn thành công!");
-  }
-  socket.emit("send message from client to server", messageText, acknowledgements);
+  };
+  socket.emit(
+    "send message from client to server",
+    messageText,
+    acknowledgements
+  );
 });
 
 socket.on("send message from server to client", (messageText) => {
   console.log(messageText);
+});
+
+// Gửi vị trí
+document.getElementById("btn-share-location").addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+  });
 });
