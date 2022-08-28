04c99c8321a55d581c68e5954590962377bfb06c
huynvn <Huy.Nguyen@navigosgroup.com>
2022-08-28 11:39:53 +0700

    Z:Chuong 01: 2: Get

--- ch01-zend/server.js
+++ ch01-zend/server.js
@@ -1,28 +1,45 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
-const COURSE = [
+const courses = [
   { id: 1, name: 'NodeJS'},
   { id: 2, name: 'ReactJS'},
+  { id: 3, name: 'PHP'},
 ]
 
 /* Update Code */
-const server = http.createServer((req, res) => {
-  // res.setHeader('Content-Type', 'application/json'); // set Header
-  // res.setHeader('X-Powered-By', 'Node.js');
-  // res.statusCode = 404;
+// const server = http.createServer((req, res) => {
+//   // res.setHeader('Content-Type', 'application/json'); // set Header
+//   // res.setHeader('X-Powered-By', 'Node.js');
+//   // res.statusCode = 404;
 
-  // Cách viết gọn. 
-  res.writeHead(404, {
-    'Content-Type' : 'application/json',
-    'X-Powered-By' : 'Node.js'
-  });
+//   // Cách viết gọn. 
+//   res.writeHead(404, {
+//     'Content-Type' : 'application/json',
+//     'X-Powered-By' : 'Node.js'
+//   });
 
-  res.end(JSON.stringify({
-    success: false,
-    error: 'NOT FOUND',
-    data: null
-  })); 
+//   res.end(JSON.stringify({
+//     success: false,
+//     error: 'NOT FOUND',
+//     data: null
+//   })); 
+// });
+
+app.get('/', (req, res) => {
+  res.send('Bạn đang tham gia NodeJS');
 });
 
-const PORT = 4000;
-server.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((course => course.id === parseInt(req.params.id)));
+  if(!course) res.status(404).send("Id không tồn tại");
+  res.send(course);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
